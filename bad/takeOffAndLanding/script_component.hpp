cb08231b7dc9da1b0b590a7e2bb0cb3d18568f0c
BadWolf64 <danstevethomas@gmail.com>
2020-04-09 17:21:24 -0300

    Trying to get everything back to normal.

--- bad/takeOffAndLanding/script_component.hpp
+++ bad/takeOffAndLanding/script_component.hpp
@@ -1,13 +1,5 @@
 #include "..\core\script_bad.hpp"
 #define COMPONENT TAKEOFFANDLANDING
-<<<<<<< HEAD
-=======
-
- #define DEBUG_MODE_FULL
- #define DISABLE_COMPILE_CACHE
- #define ENABLE_PERFORMANCE_COUNTERS
-
->>>>>>> fff42cf0644dd53b6401f5305a487d99044e3beb
 #ifdef DEBUG_ENABLED_TAKEOFFANDLANDING
     #define DEBUG_MODE_FULL
 #endif
